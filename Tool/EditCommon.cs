d546b0fd48a40d2617f2daf6e6c3a546e520f88b
U-MONSTER-WIN\stoiko <stoiko.todorov@gmail.com>
2018-06-24 19:34:46 +0300

    Added a common lib for Edits.

---
+++ Tool/EditCommon.cs
@@ -0,0 +1,61 @@
+#if UNITY_EDITOR
+
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace DMB
+{
+
+public static class EditCommon
+{
+    public static bool IsTangentHandle( int index ) {
+        return index % 3 != 0;
+    }
+
+    public static bool DrawPositionHandle( Vector3 [] path, out Vector3 drag )
+    {
+        if ( path.Length == 0 ) {
+            drag = Vector3.zero;
+            return false;
+        }
+        Bounds b = new Bounds( path[0] + new Vector3( 0.1f, 0.1f, 0.1f ), Vector3.zero );
+        if ( Tools.pivotMode == PivotMode.Center ) {
+            for ( int i = 0; i < path.Length; i++ ) {
+                var p = path[i];
+                b.Encapsulate( p );
+            }
+        }
+        Vector3 newCenter = Handles.PositionHandle( b.center, Quaternion.identity );
+        drag = newCenter - b.center;
+        return drag.sqrMagnitude > 0.001f;
+    }
+
+    public static bool DrawControlPoints( Vector3 [] path, int selIndex, bool selectedSpline,
+                                            out int newSelIndex, out int dragIndex, out Vector3 drag, 
+                                            bool detectTangents = false )
+    {
+        drag = Vector3.zero;
+        dragIndex = -1;
+        newSelIndex = -1;
+        for ( int i = 0; i < path.Length; i++ ) {
+            Vector3 selPt = path[i];
+            EditorGUI.BeginChangeCheck();
+            Vector3 newPt;
+            if ( SWUI.LinePoint( selPt, selectedSpline, false, out newPt, 
+                        selectedIndex: i == selIndex, isSecondary: detectTangents && IsTangentHandle( i ) ) ) {
+                newSelIndex = i;
+            }
+            if ( EditorGUI.EndChangeCheck() ) {
+                dragIndex = i;
+                drag = newPt - selPt;
+            }
+        }
+        return newSelIndex >= 0 || dragIndex >= 0;
+    }
+}
+
+}
+
+#endif
